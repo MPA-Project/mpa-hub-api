bf015bd1210651b7ff8728ede75d1f64115cdcb1
Alfian Rikzandi <denydirgantara94@gmail.com>
2022-03-13 12:34:30 +0700

    chore: add image fileupload

--- app/controllers/utils/uploads/TemporaryController.go
+++ app/controllers/utils/uploads/TemporaryController.go
@@ -6,10 +6,13 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
+	"github.com/h2non/bimg"
 	"myponyasia.com/hub-api/pkg/utils"
 )
 
 func UploadTemporary(c *fiber.Ctx) error {
+
+	go utils.RemoveExpiredFiles()
 
 	file, err := c.FormFile("file-upload")
 
@@ -20,11 +23,20 @@ func UploadTemporary(c *fiber.Ctx) error {
 		})
 	}
 
+	if file.Size >= (5 * 1024 * 1024) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   true,
+			"message": "File upload to big",
+		})
+	}
+
 	filename := uuid.New()
 
-	c.SaveFile(file, fmt.Sprintf("./uploads/%s-%s", filename, file.Filename))
+	modified_filename := fmt.Sprintf("./uploads/temp-%s-%s", filename, file.Filename)
 
-	info, err := os.Stat(fmt.Sprintf("./uploads/%s-%s", filename, file.Filename))
+	c.SaveFile(file, modified_filename)
+
+	info, err := os.Stat(modified_filename)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"error":   true,
@@ -32,7 +44,48 @@ func UploadTemporary(c *fiber.Ctx) error {
 		})
 	}
 
-	go utils.RemoveExpiredFiles()
+	buffer, err := bimg.Read(modified_filename)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	defer os.Remove(modified_filename)
+
+	read_image := bimg.NewImage(buffer)
+	if size, err := read_image.Size(); size.Width >= 5000 || size.Height == 5000 || err != nil {
+		if err != nil {
+			return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+				"error":   true,
+				"message": err.Error(),
+			})
+		}
+
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"error":   true,
+			"message": "Image to large",
+		})
+	}
+
+	newImage, err := read_image.Convert(bimg.JPEG)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	modified_filename = fmt.Sprintf("./uploads/%s%s", filename, ".jpeg")
+	if err := bimg.Write(modified_filename, newImage); err != nil {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
 
 	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 		"error":    false,
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gofiber/jwt/v2 v2.2.7
 	github.com/golang-jwt/jwt/v4 v4.3.0
 	github.com/google/uuid v1.3.0
+	github.com/h2non/bimg v1.1.7 // indirect
 	github.com/joho/godotenv v1.4.0
 	github.com/klauspost/compress v1.14.4 // indirect
 	github.com/mailgun/mailgun-go/v4 v4.6.0
